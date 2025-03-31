394a9b52e8888da82c289fcbd716edbbb8711b71
David8633 <davidinstituto282@gmail.com>
2025-03-31 09:10:15 +0200

    Subida de actividad 3 del boletin de javaScript

--- activadIniciacion.js
+++ activadIniciacion.js
@@ -2,3 +2,5 @@
 let numero = -4;
 /*Una variable palabra cuyo valor sea ”Hola Mundo”*/
 let palabra = "Hola Mundo";
+/*Una variable booleana cuyo nombre sea respuesta e inicialmente con valor true*/
+let respuesta = true; 
