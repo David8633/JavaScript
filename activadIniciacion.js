13b70674ba1d5b3c929cfe31bf4e770e687ed4d4
David8633 <davidinstituto282@gmail.com>
2025-03-31 09:12:31 +0200

    Subida de actividad 8 del boletin de javaScript

--- activadIniciacion.js
+++ activadIniciacion.js
@@ -10,3 +10,13 @@ const pi = 3.14;
 sin tocar la parte de la declaración de la variable.*/
 numero = 54;
 
+
+/*¿Qué pasa si modificas el valor de la variable antes de que esta sea declarada?*/
+
+/*Modifica el valor de la variable numero para que en lugar de -4 sea 54 pero sin tocar la parte de la declaración de la variable.*/
+
+
+/*Declara 2 variables y asigna los valores 5 y 2 respectivamente*/
+let variabl1 = 5;
+let variable2 = 2;
+
